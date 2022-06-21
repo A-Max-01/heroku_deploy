3ffea03ee920586cda553a528bbae6e22de3c72e
A-Max-01 <ztzsr13dz317@gmail.com>
2022-06-21 12:26:33 +0300

    deploy

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SITE_ID = 1
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
